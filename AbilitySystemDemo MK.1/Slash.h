0c27b5e18e2b3b40d71bfd0fbde0cf86851e770d
Fatecrystaline <Twistedjmonko@gmail.com>
2026-02-09 12:58:56 -0800

    Update Slash.h

--- AbilitySystemDemo MK.1/Slash.h
+++ AbilitySystemDemo MK.1/Slash.h
@@ -1,7 +1,26 @@
 #pragma once
 #include "Ability.h"
 
-class Slash : public Ability {
+/*
+    Slash (Ability)
+
+    Player ability.
+
+    Damage Formula:
+        (Strength * 3) / Target Armor
+
+    Applies Bleed to the target.
+*/
+
+class Slash : public Ability
+{
 public:
+    Slash()
+    {
+        cooldownDuration = 3.0f;
+    }
+
+    std::string GetName() const override { return "Slash"; }
+
     void Use(Entity& user, Entity& target) override;
 };
