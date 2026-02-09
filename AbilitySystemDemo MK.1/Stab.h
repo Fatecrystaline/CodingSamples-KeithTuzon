a3aa1015d21a6eb3346d13f42aba45e122ae1ee5
Fatecrystaline <Twistedjmonko@gmail.com>
2026-02-08 17:11:28 -0800

    Delete AbilitySystemDemo MK.1 directory

--- AbilitySystemDemo MK.1/Stab.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Ability.h"
-
-class Stab : public Ability {
-public:
-    void Use(Entity& user, Entity& target) override;
-};
--- AbilitySystemDemo MK.1/StatusEffect.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class Entity;
-
-class StatusEffect {
-public:
-    virtual ~StatusEffect() = default;
-    virtual void Update(Entity& target, float deltaTime) = 0;
-    virtual bool IsExpired() const = 0;
-};
