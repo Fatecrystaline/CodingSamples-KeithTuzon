93f9d6ee80e01ba5c349cd450aaa4822803e0c3b
Fatecrystaline <Twistedjmonko@gmail.com>
2026-02-09 13:12:48 -0800

    Update Ability.h

--- AbilitySystemDemo MK.1/Ability.h
+++ AbilitySystemDemo MK.1/Ability.h
@@ -1,16 +1,51 @@
+
 #pragma once
+#include <string>
+
+/*
+    Ability (Base Class)
+
+    Represents any action an Entity can perform on a target.
+
+    Responsibilities:
+    - Handle cooldown timing
+    - Execute custom behavior through polymorphism (Use)
+    
+    This allows new abilities to be added without changing Entity or main.
+*/
+
 class Entity;
 
-class Ability {
+class Ability
+{
+protected:
+    float cooldownRemaining = 0.0f;
+    float cooldownDuration = 0.0f;
+
 public:
     virtual ~Ability() = default;
 
+    virtual std::string GetName() const = 0;
     virtual void Use(Entity& user, Entity& target) = 0;
 
-    void UpdateCooldown(float deltaTime);
-    bool IsReady() const;
+    void UpdateCooldown(float dt)
+    {
+        if (cooldownRemaining > 0.0f)
+            cooldownRemaining -= dt;
+    }
 
-protected:
-    float cooldown = 3.0f;
-    float currentCooldown = 0.0f;
+    bool CanUse() const
+    {
+        return cooldownRemaining <= 0.0f;
+    }
+
+    void StartCooldown()
+    {
+        cooldownRemaining = cooldownDuration;
+    }
+
+    float GetRemainingCooldown() const
+    {
+        return cooldownRemaining;
+    }
 };
