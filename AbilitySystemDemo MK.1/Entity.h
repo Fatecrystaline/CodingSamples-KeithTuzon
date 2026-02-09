4655d7afda5c3d7523418e574b3c86bc74b89dad
Fatecrystaline <Twistedjmonko@gmail.com>
2026-02-09 13:11:04 -0800

    Update Entity.h

--- AbilitySystemDemo MK.1/Entity.h
+++ AbilitySystemDemo MK.1/Entity.h
@@ -6,12 +6,31 @@
 #include "StatusEffect.h"
 #include "Ability.h"
 
+/*
+    Entity
+
+    Represents any combat participant (Player, Skeleton, NPC, etc).
+
+    Holds:
+    - Core stats (HP, Armor, Strength, Level)
+    - Active abilities
+    - Active status effects
+
+    Responsible for:
+    - Updating cooldowns and effects every tick
+    - Preventing duplicate status effects (refresh instead of stacking)
+    - Acting as the core unit in the combat simulation
+*/
+
+
 class Entity {
 public:
     Entity(const std::string& name, int hp, int armor, int str, int level);
 
     void Update(float deltaTime);
-    void TryUseAbilities(Entity& target);
+    void TryUseAbilities(Entity& target, bool reportStatus);
+   
+    void TakeDamage(int amount);
 
     void ApplyDamage(int amount, const std::string& source = "");
     void AddStatusEffect(std::unique_ptr<StatusEffect> effect);
@@ -19,7 +38,8 @@ public:
 
     bool IsAlive() const;
     
-    int GetHP() const;
+   // HP accessor (inline)
+   int GetHP() const { return hp; }
     int GetArmor() const;
     int GetStrength() const;
     int GetLevel() const;
